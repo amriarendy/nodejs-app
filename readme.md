7905dc86b59dfe027b781e502a6579aac7d37c51
amriarendy <amriarendy90@gmail.com>
2024-01-16 23:35:58 +0700

    nodejs restfull API

---
+++ readme.md
@@ -0,0 +1,35 @@
+### cara install
+
+npm update atau npm install
+
+buat database nodejs_restfull_api atau bisa ganti database dengan di .env pada bagian ini DATABASE_URL="mysql://root:@localhost:3306/nodejs_restfull_api"
+
+jalankan perintah di terminal dengan npx prisma migrate dev
+
+cara menjalankan node /src/main.js/
+
+### node packages
+
+```json
+  "dependencies": {
+    "bcrypt": "^5.1.0",
+    "express": "^4.18.2",
+    "joi": "^17.9.2",
+    "uuid": "^9.0.0",
+    "winston": "^3.9.0"
+  }
+  "devDependencies": {
+    "@babel/preset-env": "^7.22.5",
+    "@types/bcrypt": "^5.0.0",
+    "@types/express": "^4.17.17",
+    "@types/jest": "^29.5.2",
+    "@types/supertest": "^2.0.12",
+    "@types/uuid": "^9.0.2",
+    "babel-jest": "^29.5.0",
+    "jest": "^29.5.0",
+    "prisma": "^4.15.0",
+    "supertest": "^6.3.3"
+  }
+```
+
+### Thanks &#128151;
---
+++ src/service/user-service.js
@@ -0,0 +1,75 @@
+import { PrismaClient } from "@prisma/client";
+import {
+  loginUserValidation,
+  registerUserValidation,
+} from "../validation/user-validation.js";
+import { prismaClient } from "../application/database.js";
+import { validate } from "../validation/validation.js";
+import { ResponseError } from "../error/response-error.js";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+
+const register = async (request) => {
+  const user = validate(registerUserValidation, request);
+
+  const countUser = await PrismaClient.user.count({
+    where: {
+      username: user.username,
+    },
+  });
+
+  if (countUser === 1) {
+    throw new ResponseError(400, "Username already exists");
+  }
+
+  user.password = await bcrypt.hash(user.password, 10);
+
+  return PrismaClient.user.create({
+    data: user,
+    select: {
+      username: true,
+      name: true,
+    },
+  });
+};
+
+const login = async (request) => {
+  const loginRequest = validate(loginUserValidation, request);
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      username: loginRequest.username,
+    },
+    select: {
+      username: true,
+      password: true,
+    },
+  });
+
+  if (!user) {
+    throw new ResponseError(401, "Username or password wrong");
+  }
+
+  const isPasswordValid = bcrypt.compare(loginRequest.password, user.password);
+  if (!isPasswordValid) {
+    throw new ResponseError(401, "Username or password wrong");
+  }
+
+  const token = uuid().toString();
+  return prismaClient.user.update({
+    data: {
+      token: token,
+    },
+    where: {
+      username: user.username,
+    },
+    select: {
+      token: true,
+    },
+  });
+};
+
+export default {
+  register,
+  login,
+};
